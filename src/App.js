cb49db05e23a161ddc976b3e6308709d4e7dc582
mithun shenoy <shenoymithun70@gmail.com>
2021-02-11 08:49:08 +0530

    redirecting user to home after sign in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {Switch , Route} from 'react-router-dom';
+import {Switch , Route , Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 
 import Homepage from './pages/homepage/homepage.component.jsx';
@@ -62,7 +62,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={Homepage}/>
           <Route exact path="/shop" component={ShopPage}/>
-          <Route exact path="/signin" component={SignInAndSignUpPage}/>
+          <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />) : (<SignInAndSignUpPage />)}/>
         </Switch>
       </div>
     );
@@ -70,8 +70,12 @@ class App extends React.Component {
   
 }
 
+const mapStateToProps = ({user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null,mapDispatchToProps)(App);  
+export default connect(mapStateToProps,mapDispatchToProps)(App);  
